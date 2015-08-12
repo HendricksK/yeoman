da79d2be6b8753171eab631a3e0ce605f11d1686
Kurvin Hendricks <kurvinhendricks@gmail.com>
2015-08-13 00:13:34 +0200

    First commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name gitprojectsApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the gitprojectsApp
+ */
+angular.module('gitprojectsApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name gitprojectsApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the gitprojectsApp
+ */
+angular.module('gitprojectsApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
